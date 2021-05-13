5b0238b1ada6d9789e716d200453502be05e37c2
sapandey <saurabh.pandey@pyton.travel>
2021-05-13 17:50:21 +0200

    Add project files.

---
+++ Features/RetrieveLocationData.feature
@@ -0,0 +1,16 @@
+﻿Feature: Returning location data based on country and zip code
+As a cutomer of the Zippopotam.us API
+I want to recieve location data matching the country code and zip code I supply
+I want to receive location data matching the country code and zip code I supply
+So I can use this data to auto-complete forms on my web site.
+
+Scenario: An existing country and zip code yields the correct place name
+	Given the country code US and zip code 90210
+	When I request the locations corresponding to these codes
+	Then the response contains the place name Beverly Hills
+
+Scenario: An existing country and zip code yields the right number of results
+Given the country code US and zip code 90210
+When I request the locations corresponding to these codes
+Then the response contains exactly 1 location
+
---
+++ StepDefinitions/ReturningLocationDataBasedOnCountryAndZipCodeSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SpecflowTutorial.StepDefinitions
+{
+    [Binding]
+    public class ReturningLocationDataBasedOnCountryAndZipCodeSteps
+    {
+        [Given(@"the country code US and zip code (.*)")]
+        public void GivenTheCountryCodeUSAndZipCode(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I request the locations corresponding to these codes")]
+        public void WhenIRequestTheLocationsCorrespondingToTheseCodes()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the response contains the place name Beverly Hills")]
+        public void ThenTheResponseContainsThePlaceNameBeverlyHills()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the response contains exactly (.*) location")]
+        public void ThenTheResponseContainsExactlyLocation(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
