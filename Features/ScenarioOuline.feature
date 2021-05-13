e6b52043b1568c53f4197f5833a108fa1e4adb05
sapandey <saurabh.pandey@pyton.travel>
2021-05-13 20:18:16 +0200

    recent changes-scenarioOutline

---
+++ Features/ScenarioOuline.feature
@@ -0,0 +1,14 @@
+﻿Feature:  scenario outline demo
+As a cutomer of the Zippopotam.us API
+I want to recieve location data matching the country code and zip code I supply
+I want to receive location data matching the country code and zip code I supply
+So I can use this data to auto-complete forms on my web site.
+
+Scenario Outline: Country and zip codes yield the right place names
+	Given the country code <countryCode> and zip code <zipCode>
+	When I request the location corresponding to these codes
+	Then the response contains the place name <placeName>
+	Examples: 
+	| countryCode | zipCode | placeName    |
+	| US          | 90210   | Bevery Hills |
+	| US          | 22222   | Arlington    |
--- StepDefinitions/ReturningLocationDataBasedOnCountryAndZipCodeSteps.cs
+++ StepDefinitions/ReturningLocationDataBasedOnCountryAndZipCodeSteps.cs
@@ -6,28 +6,49 @@ namespace SpecflowTutorial.StepDefinitions
     [Binding]
     public class ReturningLocationDataBasedOnCountryAndZipCodeSteps
     {
+<<<<<<< HEAD
+        [Given(@"the country code (US|CA) and zip code (.*)")]
+=======
         [Given(@"the country code (US|CA) and zip code (\d+)")]
+>>>>>>> cb7520570397172e5f0370b3f91cecdd871d7dca
         public void GivenTheCountryCodeUSAndZipCode(string countryCode, int zipCode)
         {
             
         }
+<<<<<<< HEAD
+        [When(@"I request the locations? corresponding to these codes")]
+        public void WhenIRequestTheLocationCorrespondingToTheseCodes()
+        {
+
+=======
         
         [When(@"I request the locations? corresponding to these codes")]
         public void WhenIRequestTheLocationsCorrespondingToTheseCodes()
         {
             
+>>>>>>> cb7520570397172e5f0370b3f91cecdd871d7dca
         }
-        
-        [Then(@"the response contains the place name Beverly Hills")]
-        public void ThenTheResponseContainsThePlaceNameBeverlyHills()
+
+        [Then(@"the response contains the place name (.*)")]
+        public void ThenTheResponseContainsThePlaceName(string placeName)
         {
+<<<<<<< HEAD
+
+=======
             
+>>>>>>> cb7520570397172e5f0370b3f91cecdd871d7dca
         }
-        
+
         [Then(@"the response contains exactly (.*) location")]
-        public void ThenTheResponseContainsExactlyLocation(int p0)
+        public void ThenTheResponseContainsExactlyLocation(int numLocation)
         {
+<<<<<<< HEAD
+
+=======
             
+>>>>>>> cb7520570397172e5f0370b3f91cecdd871d7dca
         }
+
+               
     }
 }
