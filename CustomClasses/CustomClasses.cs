0dd56e82a7d65651438c92d1cb82696ece2fafb7
sapandey <saurabh.pandey@pyton.travel>
2021-05-14 14:35:03 +0200

    created a class file

---
+++ CustomClasses/CustomClasses.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecflowTutorial.CustomClasses
+{
+    class locationDetails
+    {
+        public string placeName { get; set; }
+        public float longitude { get; set; }
+        public float latitude { get; set; }
+    }
+}
